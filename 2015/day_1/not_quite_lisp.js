0e100967919f74d153029fc33c45a20dc04a59bf
fgrullon <franfelix@gmail.com>
2023-07-21 08:50:28 -0400

    first day 2015 - Part [1,2]

--- 2015/day_1/not_quite_lisp.js
+++ 2015/day_1/not_quite_lisp.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 
 function read(input) {
-  let count = 0;
+  let count = 0,
+    basemen = null;
   for (let i = 0; i < input.length; i++) {
     input[i] === "(" ? count++ : count--;
+    if (count === -1 && !basemen) basemen = i + 1;
   }
-  return count;
+  return { count, basemen };
 }
 
 fs.readFile("./input.txt", "utf8", async (err, data) => {
@@ -13,7 +15,7 @@ fs.readFile("./input.txt", "utf8", async (err, data) => {
   console.log(result);
 });
 
-// Test
+// Test Part 1
 // console.log(read("(())") === 0 && read("()()") === 0);
 // console.log(
 //   read("(((") === 3 && read("(()(()(") === 3 && read("))(((((") === 3
@@ -21,3 +23,8 @@ fs.readFile("./input.txt", "utf8", async (err, data) => {
 
 // console.log(read("())") === -1 && read("))(") === -1);
 // console.log(read(")))") === -3 && read(")())())") === -3);
+
+// // Test Part 2
+// console.log(read(")"), read(")") === 1);
+
+// console.log(read("()())"), read("()())").basemen === 5);
